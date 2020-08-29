0e253632766ae6fcff108ecd56ad35f75f875e77
GitHub Action <action@github.com>
2020-08-29 18:05:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/bfergerson/sourcemarkerplugin/MyBundle.kt
+++ src/main/kotlin/com/github/bfergerson/sourcemarkerplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bfergerson.sourcemarkerplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/bfergerson/sourcemarkerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bfergerson.sourcemarkerplugin.services
+
+import com.github.bfergerson.sourcemarkerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
