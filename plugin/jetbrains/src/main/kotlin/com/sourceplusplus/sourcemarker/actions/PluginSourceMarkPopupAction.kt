043dd9cac12268d97cb0366175905dbf012851a1
Brandon Fergerson <bfergerson@apache.org>
2020-09-06 14:30:42 +0400

    some todos

--- plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/actions/PluginSourceMarkPopupAction.kt
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/actions/PluginSourceMarkPopupAction.kt
@@ -9,6 +9,15 @@ import java.util.concurrent.ThreadLocalRandom
 class PluginSourceMarkPopupAction : SourceMarkPopupAction() {
 
     override fun performPopupAction(sourceMark: SourceMark, editor: Editor) {
+        //todo: determine sourceportal context
+        //context = controller
+        //todo: get all endpoint keys for current file
+        //todo: save endpoint keys to sourcemark
+
+        //context = endpoint
+        //todo: determine endpoint key
+        //todo: save endpoint key to sourcemark
+
         //todo: use SourcePortalAPI to ensure correct view is showing
         val jcefComponent = sourceMark.sourceMarkComponent as SourceMarkJcefComponent
         if (ThreadLocalRandom.current().nextBoolean()) {
