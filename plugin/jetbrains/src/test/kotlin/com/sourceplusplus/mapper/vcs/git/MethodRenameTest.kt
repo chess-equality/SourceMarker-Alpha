ec075fe1f1b4dad631d675b2c9d60df48bad1cb7
Brandon Fergerson <brandon@srcpl.us>
2020-09-16 19:33:34 +0400

    rename test

--- plugin/jetbrains/src/test/kotlin/com/sourceplusplus/mapper/vcs/git/MethodRenameTest.kt
+++ plugin/jetbrains/src/test/kotlin/com/sourceplusplus/mapper/vcs/git/MethodRenameTest.kt
@@ -4,17 +4,83 @@ import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestC
 import jp.ac.titech.c.se.stein.PorcelainAPI
 import jp.ac.titech.c.se.stein.core.Context
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.diff.DiffConfig
+import org.eclipse.jgit.diff.DiffEntry
 import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.lib.Config
 import org.eclipse.jgit.lib.Constants
 import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.revwalk.*
 import org.intellij.lang.annotations.Language
 import org.junit.Test
 import java.io.File
+import java.util.*
+
 
 class MethodRenameTest : LightPlatformCodeInsightFixture4TestCase() {
 
     @Test
-    fun `rename java getter method`() {
+    fun `java get original method name`() {
+        if (File("/tmp/git-repo").exists()) File("/tmp/git-repo").deleteRecursively()
+        val git = Git.init().setDirectory(File("/tmp/git-repo")).call()
+        @Language("Java") val code = """
+                public class GetterMethod {
+                    private String str;
+                    public String getStr() {
+                        return str;
+                    }
+                }
+            """.trimIndent()
+        File(git.repository.directory.parent, "GetterMethod.java").writeText(code)
+        git.add().addFilepattern(".").call()
+        git.commit().setMessage("Initial commit").call()
+
+        @Language("Java") val renamedCode = """
+                public class GetterMethod {
+                    private String str;
+                    public String getStr2() {
+                        return str;
+                    }
+                }
+            """.trimIndent()
+        File(git.repository.directory.parent, "GetterMethod.java").writeText(renamedCode)
+        git.add().addFilepattern(".").call()
+        git.commit().setMessage("Renamed method").call()
+
+        val fileRepo = FileRepository("/tmp/git-repo/.git")
+        val mapper = GitRepositoryMapper(project)
+        mapper.initialize(fileRepo, fileRepo)
+        mapper.rewrite(Context.init())
+        PorcelainAPI(fileRepo).use {
+            it.resetHard()
+            it.clean()
+        }
+
+        val diffs: MutableList<DiffEntry> = ArrayList<DiffEntry>()
+        RevWalk(fileRepo).use { rw ->
+            val head: ObjectId = fileRepo.resolve(Constants.HEAD)
+            val config: Config = fileRepo.config
+            config.setBoolean("diff", null, "renames", true)
+            val dc: DiffConfig = config.get(DiffConfig.KEY)
+            val followFilter: FollowFilter = FollowFilter.create("GetterMethod.getStr2().mjava", dc)
+            followFilter.renameCallback = object : RenameCallback() {
+                override fun renamed(entry: DiffEntry) {
+                    diffs.add(entry)
+                }
+            }
+            rw.treeFilter = followFilter
+            rw.markStart(rw.parseCommit(head))
+            rw.count() //force iteration
+        }
+
+        assertEquals(1, diffs.size)
+        assertEquals(DiffEntry.ChangeType.RENAME, diffs[0].changeType)
+        assertEquals("GetterMethod.getStr().mjava", diffs[0].oldPath)
+        assertEquals("GetterMethod.getStr2().mjava", diffs[0].newPath)
+    }
+//
+//    @Test
+//    fun `java get updated method name`() {
 //        if (File("/tmp/git-repo").exists()) File("/tmp/git-repo").deleteRecursively()
 //        val git = Git.init().setDirectory(File("/tmp/git-repo")).call()
 //        @Language("Java") val code = """
@@ -50,30 +116,27 @@ class MethodRenameTest : LightPlatformCodeInsightFixture4TestCase() {
 //            it.clean()
 //        }
 //
-//        val head: ObjectId = fileRepo.resolve(Constants.HEAD)
-//        val log = git.log().add(head).addPath("GetterMethod.getStr2().mjava").call()
-//        log.forEach {
-//            println(it.fullMessage)
+//        val diffs: MutableList<DiffEntry> = ArrayList<DiffEntry>()
+//        RevWalk(fileRepo).use { rw ->
+////            rw.sort(RevSort.REVERSE);
+//            val head: ObjectId = fileRepo.resolve(Constants.HEAD)
+//            val config: Config = fileRepo.config
+//            config.setBoolean("diff", null, "renames", true)
+//            val dc: DiffConfig = config.get(DiffConfig.KEY)
+//            val followFilter: FollowFilter = FollowFilter.create("GetterMethod.getStr().mjava", dc)
+//            followFilter.renameCallback = object : RenameCallback() {
+//                override fun renamed(entry: DiffEntry) {
+//                    diffs.add(entry)
+//                }
+//            }
+//            rw.treeFilter = followFilter
+//            rw.markStart(rw.parseCommit(head))
+//            rw.count() //force iteration
 //        }
 //
-//        //git log --follow "Foo#bar().mjava"
-//        ////git log --all --full-history -- <path-to-file>
-//
-//        val finerMethodFile = File("/tmp/git-repo/GetterMethod.getStr().mjava")
-//        assertExists(finerMethodFile)
-//        assertEquals(
-//            """
-//            public
-//            String
-//            getStr
-//            (
-//            )
-//            {
-//            return
-//            str
-//            ;
-//            }
-//        """.trimIndent(), finerMethodFile.readText().trimIndent()
-//        )
-    }
+//        assertEquals(1, diffs.size)
+//        assertEquals(DiffEntry.ChangeType.RENAME, diffs[0].changeType)
+//        assertEquals("GetterMethod.getStr().mjava", diffs[0].oldPath)
+//        assertEquals("GetterMethod.getStr2().mjava", diffs[0].newPath)
+//    }
 }
