4adbeb325a9fad5e1f302a8705a722d5322fc80c
Brandon Fergerson <bfergerson@apache.org>
2020-09-17 17:21:40 +0400

    for now

--- plugin/jetbrains/src/test/kotlin/com/sourceplusplus/mapper/vcs/git/MethodRenameTest.kt
+++ plugin/jetbrains/src/test/kotlin/com/sourceplusplus/mapper/vcs/git/MethodRenameTest.kt
@@ -10,12 +10,13 @@ import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.eclipse.jgit.lib.Config
 import org.eclipse.jgit.lib.Constants
 import org.eclipse.jgit.lib.ObjectId
-import org.eclipse.jgit.revwalk.*
+import org.eclipse.jgit.revwalk.FollowFilter
+import org.eclipse.jgit.revwalk.RenameCallback
+import org.eclipse.jgit.revwalk.RevWalk
 import org.intellij.lang.annotations.Language
 import org.junit.Test
 import java.io.File
 import java.util.*
-
 
 class MethodRenameTest : LightPlatformCodeInsightFixture4TestCase() {
 
@@ -78,7 +79,7 @@ class MethodRenameTest : LightPlatformCodeInsightFixture4TestCase() {
         assertEquals("GetterMethod.getStr().mjava", diffs[0].oldPath)
         assertEquals("GetterMethod.getStr2().mjava", diffs[0].newPath)
     }
-//
+
 //    @Test
 //    fun `java get updated method name`() {
 //        if (File("/tmp/git-repo").exists()) File("/tmp/git-repo").deleteRecursively()
@@ -118,7 +119,18 @@ class MethodRenameTest : LightPlatformCodeInsightFixture4TestCase() {
 //
 //        val diffs: MutableList<DiffEntry> = ArrayList<DiffEntry>()
 //        RevWalk(fileRepo).use { rw ->
-////            rw.sort(RevSort.REVERSE);
+////            val files = DiffEntry.scan(rw.tr)
+//
+//            var diffs = git.diff()
+//                .setOldTree(prepareTreeParser(repository, oldCommit))
+//                .setNewTree(prepareTreeParser(repository, newCommit))
+//                .setPathFilter(PathFilterGroup.createFromStrings(arrayOf("new/b.txt", "b.txt")))
+//                .call()
+//            val rd = RenameDetector(repository)
+//            rd.addAll(diffs)
+//            diffs = rd.compute()
+//
+//
 //            val head: ObjectId = fileRepo.resolve(Constants.HEAD)
 //            val config: Config = fileRepo.config
 //            config.setBoolean("diff", null, "renames", true)
