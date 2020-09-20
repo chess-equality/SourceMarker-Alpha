22838663226f1abd6f55c5d75e04b768d4438805
Brandon Fergerson <bfergerson@apache.org>
2020-09-20 19:59:08 +0400

    detekt

--- portal/src/main/kotlin/com/sourceplusplus/portal/server/template/LeftNav.kt
+++ portal/src/main/kotlin/com/sourceplusplus/portal/server/template/LeftNav.kt
@@ -1,9 +1,14 @@
 package com.sourceplusplus.portal.server.template
 
-import kotlinx.html.*
+import kotlinx.html.DIV
+import kotlinx.html.FlowContent
+import kotlinx.html.div
+import kotlinx.html.style
 
-fun FlowContent.leftNav(cssClasses: String = "ui sidebar vertical left menu overlay visible very thin icon spp_blue webkit_transition",
-                        block: DIV.() -> Unit) {
+fun FlowContent.leftNav(
+    cssClasses: String = "ui sidebar vertical left menu overlay visible very thin icon spp_blue webkit_transition",
+    block: DIV.() -> Unit
+) {
     div(cssClasses) {
         style = "overflow: visible !important;"
         block()
