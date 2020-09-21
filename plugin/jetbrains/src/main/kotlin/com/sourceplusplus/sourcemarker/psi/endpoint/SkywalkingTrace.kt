c31fc7a8a2948467f2c05935db52a8fbd28f4d6d
Brandon Fergerson <bfergerson@apache.org>
2020-09-21 19:47:44 +0400

    todo

---
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/psi/endpoint/SkywalkingTrace.kt
@@ -0,0 +1,12 @@
+package com.sourceplusplus.sourcemarker.psi.endpoint
+
+/**
+ * todo: description.
+ *
+ * @since 0.0.1
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+class SkywalkingTrace
+/*
+    "org.apache.skywalking.apm.toolkit.trace.Trace"
+*/
---
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/psi/endpoint/SpringMVC.kt
@@ -0,0 +1,17 @@
+package com.sourceplusplus.sourcemarker.psi.endpoint
+
+/**
+ * todo: description.
+ *
+ * @since 0.0.1
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+class SpringMVC
+/*
+    "org.springframework.web.bind.annotation.GetMapping",
+    "org.springframework.web.bind.annotation.PostMapping",
+    "org.springframework.web.bind.annotation.PutMapping",
+    "org.springframework.web.bind.annotation.DeleteMapping",
+    "org.springframework.web.bind.annotation.PatchMapping",
+    "org.springframework.web.bind.annotation.RequestMapping"
+*/
