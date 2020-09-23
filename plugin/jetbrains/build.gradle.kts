751844289a53b760522d5ca527f21cdffcedfb1e
Brandon Fergerson <bfergerson@apache.org>
2020-09-23 22:11:56 +0400

    backend/frontend

--- plugin/jetbrains/build.gradle.kts
+++ plugin/jetbrains/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(project(":mentor"))
     implementation(project(":monitor:skywalking"))
     implementation(project(":protocol"))
-    implementation(project(":portal"))
+    implementation(project(":portal:backend"))
 
     val vertxVersion = "3.9.2"
     implementation("com.github.sh5i:git-stein:v0.5.0")
